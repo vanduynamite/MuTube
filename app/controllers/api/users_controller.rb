14670acb33a8e7b4cc7d477003fb2d4fe0c9a500
Paul van Duyn <paul.vanduyn@gmail.com>
2018-11-22 20:53:23 -0500

    fix bug on user create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -28,10 +28,10 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.user_image_url = IMAGE_COLORS.sample
-
+    debugger
     if @user.save
       login!(@user)
-      render 'api/users/show.json.jbuilder'
+      render 'api/users/session.json.jbuilder'
     else
       render json: @user.errors.full_messages, status: 422
     end
